3cae6b058f54bd4c0411e22ed63969cda3ee9b78
Chonlada Keankham <chonlada.kkc@gmail.com>
2025-07-07 11:36:56 +0700

    ..

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -27,6 +27,7 @@ export const fileConfig = {
 
 export const redisConfig = {
   useRedis: process.env.USE_REDIS === 'true',
+  url: process.env.REDIS_URL || 'rediss://default:AdnWAAIjcDE3N2IzZGY0NzdlYWM0MzI0OWNjYjM4YjJkOGM5NDRlYXAxMA@inviting-gull-55766.upstash.io:6379',
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
   password: process.env.REDIS_PASSWORD,
